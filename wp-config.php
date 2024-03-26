55a4d1a82f82b29190b9cef57fca26e3053017c6
vinnyrags <vincentpasqualeragosta@gmail.com>
2024-03-26 01:43:30 -0400

    remove WpConfig.php helper class (temporarily)

--- wp-config.php
+++ wp-config.php
@@ -27,10 +27,6 @@ if (file_exists(__DIR__ . '/local-config.php')) {
     include(__DIR__ . '/local-config.php');
 }
 
-//if (class_exists('\DevAnime\Support\WPConfig')) {
-//    new DevAnime\Support\WPConfig(__DIR__);
-//}
-
 // Turn off Theme File Editor
 define( 'DISALLOW_FILE_EDIT', true );
 define( 'DISALLOW_FILE_MODS', true );
