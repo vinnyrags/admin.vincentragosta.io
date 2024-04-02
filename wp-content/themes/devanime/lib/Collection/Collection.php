3abdec493f8a19261df34cce98b3d47c552c1e5c
vinnyrags <vincentpasqualeragosta@gmail.com>
2024-04-02 01:41:02 -0400

    remove devanime theme from project + install via composer

--- wp-content/themes/devanime/lib/Collection/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace DevAnime\Collection;
-
-/**
- * interface Collection
- * @package DevAnime\Collection
- */
-interface Collection extends \ArrayAccess, \IteratorAggregate, \Countable {}
