d43876ae2a074b5f41cee3d0d2cbc4b9d147cddc
vinnyrags <vincentpasqualeragosta@gmail.com>
2024-04-07 22:21:45 -0400

    create child theme named vincentragosta, parent theme devanime

--- wp-content/themes/vincentragosta/functions.php
+++ wp-content/themes/vincentragosta/functions.php
@@ -1,16 +1,3 @@
 <?php
-//
-//add_filter( 'wp_prepare_themes_for_js', 'hide_parent_theme' );
-//function hide_parent_theme( $prepared_themes ) {
-//    $parent_theme = 'parent-theme-folder-name'; // Replace 'parent-theme-folder-name' with the actual folder name of your parent theme
-//
-//    if ( isset( $prepared_themes['themes'][ $parent_theme ] ) ) {
-//        unset( $prepared_themes['themes'][ $parent_theme ] );
-//    }
-//
-//    return $prepared_themes;
-//}
 
-//159.203.165.207 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMnaJE1OOIKKt3kxbct5YqHrlEsaCyuz76QYdOeOlJkN
-//159.203.165.207 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCP8YeZBcPmvWatkBFdUu2RnclHBcLmB0dUgmi7EMZhAk/OplOwFAea2alrnZk/fajFLfhTVR1nnIeIdgmJlEKNA9uqz5E5TKxIOXgum/3D1y75zWEaMbHTfEX/yLpnd1ZEk+jz2bieb+ypNamrUUSCwC4mtFkXodl97dOXGCUBDw/$
-//159.203.165.207 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHiQ8WaRN+Fo1WOtr9m8lZr4C0PVltPL+s3EQ6ssb4J8sNCPhAX/XGz09+k6W8uQs90R7z+xOHKF+ZrwOl/V9Q8=
+new ChildTheme\Theme();
